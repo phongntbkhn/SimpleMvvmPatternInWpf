ee21d7a11ec15208113e91f03065632eddb4af9a
trongphong1410 <61769631+ChickenIT-TP@users.noreply.github.com>
2024-05-14 06:26:18 +0700

    cmm

---
+++ Model/User.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMvvmPatternInWpf.Model
+{
+    public class User
+    {
+        private int userId;
+        private string fullName = "";
+        private string email = "";
+        private string phoneNumber = "";
+        private string address = "";
+
+        public int UserId
+        {
+            get
+            {
+                return userId;
+            }
+            set
+            {
+                userId = value;
+                OnPropertyChanged(nameof(UserId));
+            }
+        }
+
+        public string FullName
+        {
+            get { return fullName; }
+            set
+            {
+                fullName = value;
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+
+            set
+            {
+                email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set
+            {
+                phoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                OnPropertyChanged(nameof(Address));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = null!;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,64 @@
+﻿using SimpleMvvmPatternInWpf.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleMvvmPatternInWpf.ViewModel
+{
+    public class UserViewModel
+    {
+        private List<User> _users = new List<User>();
+        public UserViewModel()
+        {
+            _users = GetInitialUsers();
+        }
+
+        private List<User> GetInitialUsers()
+        {
+            return new List<User>()
+            {
+                new User() {UserId = 1, FullName = "Ngô Trọng Phong", Email = "phong@gmail.com", PhoneNumber = "0123999999", Address = "Hải Dương"},
+                new User() {UserId = 2, FullName = "Nguyễn Thị Bình", Email = "binh@gmail.com", PhoneNumber = "0123888888", Address = "Hà Nội"},
+                new User() {UserId = 3, FullName = "Ngô Minh Châu", Email = "minhchau@gmail.com", PhoneNumber = "0123777777", Address = "Hải Dương"},
+            };
+        }
+
+        public List<User> Users
+        {
+            get { return _users; }
+            set { _users = value; }
+        }
+
+        private ICommand updateStatus;
+
+        public ICommand UpdateCommand
+        {
+            get
+            {
+                if (updateStatus == null)
+                    updateStatus = new Updater();
+                return updateStatus;
+            }
+
+            set
+            {
+                updateStatus = value;
+            }
+        }
+
+        private class Updater : ICommand
+        {
+            public bool CanExecute(object parameter) => true;
+
+            public event EventHandler? CanExecuteChanged;
+
+            public void Execute(object? parameter)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
